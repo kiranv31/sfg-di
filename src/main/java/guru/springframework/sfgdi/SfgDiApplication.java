6d440e2da83a09643ac89e8489e796f524a3c456
Kiran <kiran.verma.cs@gmail.com>
2020-03-19 01:57:58 +0530

    Initial commit

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.sfgdi.controllers.MyController;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		MyController ctrl = (MyController)ctx.getBean("myController");
+		ctrl.sayHello();
+	}
+
+}
